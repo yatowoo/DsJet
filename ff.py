7c5e148a8278e58d4934e4ff28c3e3b874ec9479
yatowoo <torrence@mail.ustc.edu.cn>
2022-03-30 23:53:47 +0800

    FF - Merge inv. mass for jet pT bins

---
+++ ff.py
@@ -0,0 +1,74 @@
+#!/bin/env python3
+
+import ROOT
+from machine_learning_hep.fitting.fitters import FitAliHF
+
+f = ROOT.TFile("../DsJet-pre/pp_data/masshisto.root")
+
+# pt_cand: [3,4,6,8, 12, 24]
+# jet_pt : [5, 7, 15, 35]
+# xgboost: [0.96, 0.98, 0.94, 0.97, 0.96]
+
+pt_jet_l = [5, 7, 15]
+pt_jet_u = [7, 15, 35]
+pt_cand_l = [3,4,6,8, 12]
+pt_cand_u = [4,6,8, 12, 24]
+prob = [0.98, 0.98, 0.94, 0.97, 0.96]
+
+c = ROOT.TCanvas("c1","Fitting",2400,800)
+c.Divide(3,1)
+c.Draw()
+
+# Check fitters
+  # Parameters
+fit_pars = {'bkg_func_name': 2,
+		 'fit_range_low': 1.75,
+		 'fit_range_up': 2.15,
+		 'fix_mean': False,
+		 'fix_sec_mean': False,
+		 'fix_sec_sigma': False,
+		 'fix_sigma': False,
+		 'include_sec_peak': True,
+		 'likelihood': False,
+		 'mean': 1.969,
+		 'n_sigma_sideband': 4,
+		 'rebin': 6,
+		 'rel_sigma_bound': 0.5,
+		 'sec_mean': 1.869,
+		 'sec_sigma': 0.91,
+		 'sig_func_name': 0,
+		 'sigma': 0.011,
+ 'use_sec_peak_rel_sigma': True}
+
+# hmass_[i]                                # pt_cand bin
+# hmasspt_cand12_24_0.96pt_jet_7.00_15.00  # TH1, inv. mass
+# hzvsmasspt_cand12_24_0.96pt_jet_15.00_35.00   # TH2, z vs inv. mass
+# hmasspt_cand3_4_0.98pt_jet_5.00_7.00
+# hmasspt_cand4_6_0.98pt_jet_5.00_7.00
+def histname(prefix, ptCandL, ptCandU, ptJetL, ptJetU, probVal):
+  return f'{prefix}pt_cand{ptCandL:.0f}_{ptCandU:.0f}_{probVal:.2f}pt_jet_{ptJetL:.2f}_{ptJetU:.2f}'
+
+fitters = []
+for i in range(len(pt_jet_l)):
+  fitters.append({})
+  fitters[i]["pt_jet_l"] = pt_jet_l[i]
+  fitters[i]["pt_jet_u"] = pt_jet_u[i]
+  fitters[i]["hmass"] = None
+  for j in range(len(pt_cand_l)):
+    hname = histname('hmass', pt_cand_l[j], pt_cand_u[j], pt_jet_l[i], pt_jet_u[i], prob[j])
+    if(not fitters[i]["hmass"]):
+      try:
+        fitters[i]["hmass"] = f.Get(hname).Clone(f'hmass_jet_{pt_jet_l[i]:.0f}_{pt_jet_u[i]:.0f}')
+      except Exception:
+        print(hname)
+        exit()
+    else:
+      fitters[i]["hmass"].Add(f.Get(hname))
+  fitters[i]["core"] = FitAliHF(fit_pars, histo=fitters[i]["hmass"])
+  fitters[i]["core"].fit()
+  c.cd(i+1)
+  fitters[i]["core"].draw(ROOT.gPad)
+
+c.SaveAs("test.pdf")
+
+f.Close()
