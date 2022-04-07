9f92b70e1ab8be013977539349827af9579dad76
yatowoo <torrence@mail.ustc.edu.cn>
2022-04-07 23:21:04 +0800

    FF - eff. correction

--- ff.py
+++ ff.py
@@ -7,6 +7,8 @@ from copy import deepcopy
 from ctypes import c_double
 
 f = ROOT.TFile("../DsJet-pre/pp_data/masshisto.root")
+fEff = ROOT.TFile("../DsJet-pre/pp_mc_prodD2H/effhisto.root")
+fOut = ROOT.TFile("DsJet-ff.root","RECREATE")
 
 # pt_cand: [3,4,6,8, 12, 24]
 # jet_pt : [5, 7, 15, 35]
@@ -22,8 +24,8 @@ c = ROOT.TCanvas("c1","Fitting",2400,1600)
 c.Divide(3,2)
 c.Draw()
 
-cnew = ROOT.TCanvas("c2","Fitting",2400,1600)
-cnew.Divide(3,2)
+cnew = ROOT.TCanvas("c2","Fitting",2400,3200)
+cnew.Divide(3,4)
 cnew.Draw()
 
 # Check fitters
@@ -187,6 +189,26 @@ for i in range(len(pt_jet_l)):
   fitters[i]["hSBright"] = DrawRegion(fitters[i]["core"].histo, f'hSBright_{i}',
     fitters[i]["sideband_right_l"], fitters[i]["sideband_right_u"],
     ROOT.kRed, 3354)
+  # Efficiency weight
+  nameSuffix = f'_pt_jet_{fitters[i]["pt_jet_l"]:.2f}_{fitters[i]["pt_jet_u"]:.2f}'
+    # Prompt
+  h_gen_pr = fEff.Get('h_gen_pr' + nameSuffix)
+  h_sel_pr = fEff.Get('h_sel_pr' + nameSuffix)
+  fitters[i]["heff_pr"] = h_sel_pr.Clone("heff_pr_" + nameSuffix)
+  fitters[i]["heff_pr"].SetTitle('D_{s} reco. eff. in |y|<0.5')
+  fitters[i]["heff_pr"].SetDirectory(0)
+  fitters[i]["heff_pr"].Divide(h_gen_pr)
+  cnew.cd(7+i)
+  fitters[i]["heff_pr"].SetLineColor(ROOT.kRed)
+  fitters[i]["heff_pr"].Draw()
+    # Non-Prompt / Feed-down
+  h_gen_fd = fEff.Get('h_gen_fd' + nameSuffix)
+  h_sel_fd = fEff.Get('h_sel_fd' + nameSuffix)
+  fitters[i]["heff_fd"] = h_sel_fd.Clone("heff_fd_" + nameSuffix)
+  fitters[i]["heff_fd"].SetDirectory(0)
+  fitters[i]["heff_fd"].Divide(h_gen_fd)
+  fitters[i]["heff_fd"].SetLineColor(ROOT.kBlack)
+  fitters[i]["heff_fd"].Draw("same")
   # Raw FF - hzvsmass
   fitters[i]["hzvsmass"] = []
   for j in range(len(pt_cand_l)):
@@ -194,36 +216,50 @@ for i in range(len(pt_jet_l)):
     hzname = histname('hzvsmass', pt_cand_l[j], pt_cand_u[j], pt_jet_l[i], pt_jet_u[i], prob[j])
     fitters[i]["hzvsmass"].append(f.Get(hzname).Clone(f"hzmass_{i}_{j}"))
     h2tmp = fitters[i]["hzvsmass"][j]
+    recoEff = fitters[i]["heff_pr"].GetBinContent(j+1)
+    if(recoEff < 0.0 or recoEff > 1.0):
+      print('[X] Error: invalid efficiency found')
+      exit()
     # Signal
     h2tmp_profileZ = h2tmp.ProjectionY(f"_pz_sig",
       h2tmp.FindBin(fitters[i]["signal_l"]),
       h2tmp.FindBin(fitters[i]["signal_u"]))
     if(not fitters[i].get("hz_signal")):
       fitters[i]["hz_signal"] = h2tmp_profileZ.Clone(f"hz_signal_{i}")
+      fitters[i]["hz_signal_corrected"] = h2tmp_profileZ.Clone(f"hz_signal_corrected_{i}")
+      fitters[i]["hz_signal_corrected"].Scale(1. / recoEff)
     else:
       fitters[i]["hz_signal"].Add(h2tmp_profileZ)
+      fitters[i]["hz_signal_corrected"].Add(h2tmp_profileZ, 1./recoEff)
     # Sideband left
     h2tmp_profileZ = h2tmp.ProjectionY(f"_pz_sbl",
       h2tmp.FindBin(fitters[i]["sideband_left_l"]),
       h2tmp.FindBin(fitters[i]["sideband_left_u"]))
     if(not fitters[i].get("hz_sidebandL")):
       fitters[i]["hz_sidebandL"] = h2tmp_profileZ.Clone(f"hz_sidebandL_{i}")
+      fitters[i]["hz_sidebandL_corrected"] = h2tmp_profileZ.Clone(f"hz_sidebandL_{i}_corrected")
+      fitters[i]["hz_sidebandL_corrected"].Scale(1. / recoEff)
     else:
       fitters[i]["hz_sidebandL"].Add(h2tmp_profileZ)
+      fitters[i]["hz_sidebandL_corrected"].Add(h2tmp_profileZ, 1./recoEff)
     # Sideband right
     h2tmp_profileZ = h2tmp.ProjectionY(f"_pz_sbr",
       h2tmp.FindBin(fitters[i]["sideband_right_l"]),
       h2tmp.FindBin(fitters[i]["sideband_right_u"]))
     if(not fitters[i].get("hz_sidebandR")):
       fitters[i]["hz_sidebandR"] = h2tmp_profileZ.Clone(f"hz_sidebandR_{i}")
+      fitters[i]["hz_sidebandR_corrected"] = h2tmp_profileZ.Clone(f"hz_sidebandL_{i}_corrected")
+      fitters[i]["hz_sidebandR_corrected"].Scale(1. / recoEff)
     else:
       fitters[i]["hz_sidebandR"].Add(h2tmp_profileZ)
+      fitters[i]["hz_sidebandL_corrected"].Add(h2tmp_profileZ, 1./recoEff)
   fitters[i]["hz_signal"].SetLineColor(ROOT.kBlue)
   fitters[i]["hz_sideband"] = fitters[i]["hz_sidebandR"].Clone(f"hz_sideband_{i}")
   fitters[i]["hz_sideband"].Add(fitters[i]["hz_sidebandL"])
   fitters[i]["hz_sideband"].SetLineColor(ROOT.kRed)
   cnew.cd(4+i)
   fitters[i]["hz_signal"].GetYaxis().SetRangeUser(0, 2 * fitters[i]["hz_signal"].GetMaximum())
+  fitters[i]["hz_signal"].SetTitle("Raw z-distribution, bkg. subtraction using sideband")
   fitters[i]["hz_signal"].Draw()
   fitters[i]["hz_sideband"].Draw('same')
     # Substracted
@@ -235,8 +271,18 @@ for i in range(len(pt_jet_l)):
   fitters[i]["hz_sub"].SetLineColor(ROOT.kBlack)
   fitters[i]["hz_sub"].SetLineWidth(2)
   fitters[i]["hz_sub"].Draw("same")
-  # Efficiency weight
-
+    # Corrected
+  fitters[i]["hz_sideband_corrected"] = fitters[i]["hz_sidebandL_corrected"].Clone(f"hz_sideband_corrected_{i}")
+  fitters[i]["hz_sideband_corrected"].Add(fitters[i]["hz_sidebandR_corrected"])
+  fitters[i]["hz_sub_corrected"] = fitters[i]["hz_signal_corrected"].Clone(f"hz_sub_corrected_{i}")
+  fitters[i]["hz_sub_corrected"].Add(fitters[i]["hz_sideband_corrected"], -1 * area_scale)
+  fitters[i]["hz_sub_corrected"].SetTitle("Corrected z-distribution")
+  fitters[i]["hz_sub_corrected"].SetLineColor(ROOT.kGreen+3)
+  fitters[i]["hz_sub_corrected"].SetLineWidth(2)
+  cnew.cd(10+i)
+  fitters[i]["hz_sub_corrected"].Draw("")
 cnew.SaveAs("test.pdf")
 
 f.Close()
+fEff.Close()
+fOut.Close()
