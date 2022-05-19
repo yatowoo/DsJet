3a57eb99cc75d42afeb695da3b1cf264bf02b24f
yatowoo <torrence@mail.ustc.edu.cn>
2022-05-19 20:32:27 +0800

    MC - find HF cand. and decay products

--- readAOD.py
+++ readAOD.py
@@ -1,4 +1,4 @@
-#!/bin/env python3
+#!/usr/bin/env python3
 
 # AliPhysics read AOD event
 import ROOT
@@ -20,14 +20,34 @@ mcH = aod.GetList().FindObject(mcHeader)
 candBranch = 'Charm3Prong'
 candarray = aod.GetList().FindObject(candBranch)
 
-pdgCand = 431 # Ds
-for ev in aodChain:
+pdgCand = [431, 4122] # Ds, Lc
+idxCand = []
+
+def MCParticleName(mcpart):
+  if(mcpart is None):
+    return '[IP]'
+  try:
+    return ROOT.TDatabasePDG.Instance().GetParticle(mcpart.GetPdgCode()).GetName()
+  except ReferenceError:
+    return str(mcpart.GetPdgCode())
+
+def FindMCdaughters(array, mcpart, daughter_vec):
+  for i in range(mcpart.GetNDaughters()):
+    daughterLabel = mcpart.GetDaughterLabel(i)
+    if(daughterLabel == -1): continue
+    daughter = array.At(daughterLabel)
+    if(daughter is None): continue
+    daughter_vec.append(daughterLabel)
+    if(not daughter.IsPhysicalPrimary()):
+      FindMCdaughters(array, daughter, daughter_vec)
+
+for i, ev in enumerate(aodChain):
   candFound = False
-  for mcPar in mcarray:
-    if(abs(mcPar.GetPdgCode()) != pdgCand): continue
-    mcPar.Print()
-    mcarray[mcPar.GetDaughterFirst()].Print()
-    mcarray[mcPar.GetDaughterLast()].Print()
-    candFound = True
-    break
+  for j, mcPar in enumerate(mcarray):
+    if(abs(mcPar.GetPdgCode()) not in pdgCand): continue
+    mother = None if mcPar.GetMother() == -1 else mcarray[mcPar.GetMother()]
+    daughter_vec = []
+    FindMCdaughters(mcarray, mcPar, daughter_vec)
+    print(f'{i:06d}\t{MCParticleName(mother)} -> {MCParticleName(mcPar)} -> ', end='')
+    print(', '.join([MCParticleName(mcarray[id]) for id in daughter_vec]))
   if(candFound): break
