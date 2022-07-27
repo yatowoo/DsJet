2e6135311e2efed2c4d94c155e8e44a6ee43f222
yatowoo <torrence@mail.ustc.edu.cn>
2022-07-27 13:54:11 +0800

    GDM - support XML

--- run/download_train.py
+++ run/download_train.py
@@ -24,6 +24,7 @@ import multiprocessing as mp
 import argparse
 from pprint import pprint
 from copy import deepcopy
+import defusedxml.ElementTree as xml
 
 def query_yes_no(question, default="yes"):
   valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
@@ -72,14 +73,13 @@ def path_local(path_raw):
 def path_alien(path_raw):
   return path_raw if path_raw.startswith('alien://') else f'alien://{path_raw}'
 
-def find_alien(path_dir, pattern='', args='', **subproc_args):
+def find_alien(path_dir, pattern='', args=[], **subproc_args):
   """
   """
-  cmd = ['alien_find', path_alien(path_dir), pattern, args]
+  cmd = ['alien_find', path_alien(path_dir), pattern] + args
   print_cmd(cmd)
   proc = subprocess.run(cmd, stdout=subprocess.PIPE, **subproc_args)
-  ret = proc.stdout.decode('utf-8')
-  return ret.split()
+  return proc.stdout.decode('utf-8')
 
 def find_local(path_dir, pattern_name='*.root',typefile='f',**find_args):
   """Find files with name pattern
@@ -87,7 +87,7 @@ def find_local(path_dir, pattern_name='*.root',typefile='f',**find_args):
   cmd = ['find',path_dir, '-name',pattern_name]
   print_cmd(cmd)
   proc = subprocess.run(cmd, stdout=subprocess.PIPE)
-  return proc.stdout.decode('utf-8').split()
+  return proc.stdout.decode('utf-8')
 
 def ls_alien(path_dir, pattern=''):
   """List files under Alien directory
@@ -209,7 +209,8 @@ class GridDownloaderManager:
   - mp_report_dt: int, monitor report progress per N seconds (Default: 30)
   """
   def __init__(self, train_name, train_id, path_local, file_list, **gdm_args) -> None:
-    self.train_name = train_name
+    self.train_name = train_name # PWGHF/HF_TreeCreator
+    self.train_pwg, self.train_subname = self.train_name.split('/')
     self.train_id = train_id
     self.train_no = train_id.split('_')[0]
     self.path_local = path_local
@@ -223,6 +224,7 @@ class GridDownloaderManager:
     self.child_list = gdm_args.get('child_list', None)
     if self.child_list is not None:
       self.child_list = [ f'child_{i}' for i in self.child_list]
+    self.enable_xml = gdm_args.get('enable_xml', False)
   def read_env(self, path_envfile=None) -> dict:
     """
     """
@@ -277,7 +279,53 @@ class GridDownloaderManager:
     self.env_alien = f'{self.train_alien}/{self.child_jobs[0]}/env.sh'
     download_alien(self.env_alien, self.env_local)
     return self.read_env()
-  def generate_path(self) ->  None:
+  def generate_path_xml(self):
+    """
+    """
+    for child_id,cfg in self.child_conf.items():
+      print(f'[+] Generating file list for {child_id} : ')
+      # Filelist Path
+      os.system('mkdir -p ' + cfg['path_local'])
+      child_filelist_suffix = f'{self.train_subname}-{self.train_id}-{child_id}.xml'
+      child_filelist_alien = f'TMP/{child_filelist_suffix}'
+      os.system('alien_mkdir TMP/') # ? CWD
+      child_filelist = f'{cfg["path_local"]}/{child_filelist_suffix}'
+      self.child_conf[child_id]['filelist'] = child_filelist
+      if check_local_file(child_filelist, self.flag_overwrite):
+        print(f'>>> File list found - {child_filelist}')
+        continue
+      # Generate
+      xml_filelist = None
+      file_size_total = 0
+      n_files = 0
+      for file_name in self.file_list:
+        xml_node = xml.fromstring(find_alien(cfg['path_alien'],
+          cfg['file_pattern'] + file_name,
+          args=['-x', '-']))
+        # alien-collection-[event-file]
+        result = xml_node.find('collection').findall('event')
+        n_files += len(result)
+        for entry in result:
+          entry = entry.find('file').attrib # entry[0]
+          filename_alien = entry['lfn']
+          file_size_total += int(entry['size']) # Bytes
+          fname_args = filename_alien.split('/')
+          entry['path_alien'] = filename_alien
+          entry['run'] = fname_args[5]
+          entry['subjob'] = fname_args[-2]
+          entry['path_local'] = os.path.realpath(cfg['path_local'] + f'/{entry["run"]}/{entry["subjob"]}/{file_name}')
+          os.system('mkdir -p ' + entry['path_local'])
+        # Append
+        if xml_filelist:
+          xml_filelist.append(deepcopy(result))
+        else:
+          xml_filelist = deepcopy(xml_node)
+      # Save
+      print(f' > N files found = {n_files} ({repr(self.file_list)}) - Total size : {file_size_total/(1024**3):.3f} GB')
+      print(f' > Example : {entry["path_alien"]} {entry["path_local"]}')
+      with open(child_filelist, 'w') as f:
+        f.write(xml.tostring(xml_filelist).decode('utf-8'))
+  def generate_path_txt(self) ->  None:
     for child_id,cfg in self.child_conf.items():
       print(f'[+] Generating file list for {child_id} : ')
       #mkdir
@@ -292,7 +340,7 @@ class GridDownloaderManager:
       n_files = 0
       with open(child_filelist,'w') as fout:
         for file_name in self.file_list:
-          ret = find_alien(cfg['path_alien'], cfg['file_pattern'] + file_name)
+          ret = find_alien(cfg['path_alien'], cfg['file_pattern'] + file_name).split()
           n_files += len(ret)
           for fname in ret:
             if not fname: continue
@@ -309,15 +357,33 @@ class GridDownloaderManager:
     """
     """
     cfg = self.child_conf[child_id]
-    files_local = find_local(cfg['path_local'],pattern_name='*.root')
+    files_local = find_local(cfg['path_local'],pattern_name='*.root').split()
     nfiles_local = len(files_local)
     return nfiles_local
+  def read_filelist(self, child_id):
+    """
+    """
+    filelist_name = self.child_conf[child_id]['filelist']
+    filelist = []
+    f_filelist = open(filelist_name,'r')
+    if not self.enable_xml:
+      for entry in f_filelist.readlines():
+        if not entry: continue
+        filelist.append({})
+        filelist[-1]['path_alien'], filelist[-1]['path_local'] = entry.split()
+      f_filelist.close()
+      return filelist
+    # XML processing
+    et = xml.parse(open(filelist_name,'r'))
+    for coll in et.findall('collection'):
+      for ev in coll.findall('event'):
+        filelist.append(deepcopy(ev[0].attrib))
+    return filelist
   def process_child(self, child_id) -> bool:
     """
     """
     cfg = self.child_conf[child_id]
-    f_filelist = open(cfg['filelist'],'r')
-    filelist = f_filelist.readlines()
+    filelist =self.read_filelist(child_id)
     nfiles_alien = len(filelist)
     nfiles_local = self.validate_child(child_id)
     print(f'[-] Processing {child_id} - {nfiles_alien} files')
@@ -331,10 +397,7 @@ class GridDownloaderManager:
     errfile = f'{cfg["path_local"]}/download_train_stderr_{child_id}_{timestamp}.log'
     job_arguments = []
     for file_id, file_single in enumerate(filelist):
-      file_alien = file_single.split()[0]
-      file_local = file_single.split()[1]
-      job_arguments.append({'source':file_alien, 'target':file_local,'args':'-f -retry 3', 'job_id':file_id, 'job_n':nfiles_alien, 'debug':self.flag_debug, 'log_mq':log_mq})
-    f_filelist.close()
+      job_arguments.append({'source':file_single['path_alien'], 'target':file_single['path_local'],'args':'-f -retry 3', 'job_id':file_id, 'job_n':nfiles_alien, 'debug':self.flag_debug, 'log_mq':log_mq})
     # Start multithreading
     print(f'>>> Downloading...\n>>> Log : {logfile}\n>>> Err : {errfile}')
     if self.mp_jobs < 1:
@@ -346,7 +409,7 @@ class GridDownloaderManager:
       listener.get() # clear MQ
     # Validation
       # TODO: integrity by size / cksum / md5
-    files_local = find_local(cfg['path_local'],pattern_name='*.root')
+    files_local = find_local(cfg['path_local'],pattern_name='*.root').split()
     nfiles_local = len(files_local)
     files_fail = [] # grep / readlines by open
     with open(logfile,'r') as f_log:
@@ -358,7 +421,10 @@ class GridDownloaderManager:
     return True
   def start(self):
     self.get_env()
-    self.generate_path()
+    if self.enable_xml:
+      self.generate_path_xml()
+    else:
+      self.generate_path_txt()
     # Select children
     if self.child_list is None:
       self.child_list = self.child_conf.keys() # all
@@ -379,12 +445,13 @@ if __name__ == '__main__':
   parser.add_argument('-c', '--children', nargs='+', help='Specify children list to download, None is ALL.')
   parser.add_argument('--step', type=int, default=20, help='Specify progress report per N jobs')
   parser.add_argument('--dt', type=int, default=30, help='Specify progress report per N seconds')
+  parser.add_argument('--xml', default=False, action='store_true', help='Generate filelist in XML format (required by intergrity check)')
   # Save dir.: <path_local>[/<AliPhysics_tag>/<data_or_mc_production>/<train_name>/unmerged/child_<ID>]
   # Job dir.: [RunNumber]/[JobID]
   args, unknown =  parser.parse_known_args()
   if unknown:
     print(f'[+] Unknown arguments : {unknown}')
-  adm = GridDownloaderManager(args.train, args.train_id, args.path_local, args.files.split(','), overwrite=args.overwrite, debug=args.verbose, mp_jobs=args.jobs, child_list=args.children, mp_report_step=args.step, mp_report_dt=args.dt) # Alien Download Manager
+  adm = GridDownloaderManager(args.train, args.train_id, args.path_local, args.files.split(','), overwrite=args.overwrite, debug=args.verbose, mp_jobs=args.jobs, child_list=args.children, mp_report_step=args.step, mp_report_dt=args.dt, enable_xml=args.xml) # Alien Download Manager
   # Debug
   #print(args)
   adm.start()
